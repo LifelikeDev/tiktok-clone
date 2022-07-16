7f8d6c89a727cdfa2e58d7b39b39fdbd772b1e55
dev-navian <primeguyy@gmail.com>
2022-07-16 22:23:33 +0000

    play, pause and mute video

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -14,6 +14,31 @@ interface VideoCardProps {
 }
 
 const VideoCard: NextPage<VideoCardProps> = ({ post, isShowingOnHome }) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [isHover, setIsHover] = useState(false);
+  const [isVideoPlaying, setIsVideoPlaying] = useState(false);
+  const [isVideoMuted, setIsVideoMuted] = useState(false);
+
+  const handleVideoPress = () => {
+    if (isVideoPlaying) {
+      videoRef?.current?.pause();
+      setIsVideoPlaying(false);
+    } else {
+      videoRef?.current?.play();
+      setIsVideoPlaying(true);
+    }
+  };
+
+  const handleVideoSound = () => {
+    if (isVideoMuted) {
+      videoRef!.current!.muted = false;
+      setIsVideoMuted(false);
+    } else {
+      videoRef!.current!.muted = true;
+      setIsVideoMuted(true);
+    }
+  };
+
   return (
     <div className="flex flex-col border-b-2 border-gray-200 pb-6">
       <div>
@@ -39,9 +64,55 @@ const VideoCard: NextPage<VideoCardProps> = ({ post, isShowingOnHome }) => {
                 {post.postedBy.userName}
                 <GoVerified className="text-[#F51997] text-md" />
               </p>
-                <p className="capitalize text-gray-500 text-xs font-medium hidden md:block">{post.postedBy.userName}</p>
+              <p className="capitalize text-gray-500 text-xs font-medium hidden md:block">
+                {post.postedBy.userName}
+              </p>
             </div>
           </Link>
+        </div>
+      </div>
+
+      <div className="lg:ml-20 flex gap-4 relative">
+        <div
+          className="rounded-3xl"
+          onMouseEnter={() => {
+            setIsHover(true);
+          }}
+          onMouseLeave={() => {
+            setIsHover(false);
+          }}
+        >
+          <Link href="">
+            <video
+              ref={videoRef}
+              src={post.video.asset.url}
+              loop
+              className="cursor-pointer lg:w[600px] h-[300px] md:h-[350px] w-[300px] rounded-2xl bg-gray-100"
+            ></video>
+          </Link>
+
+          {isHover && (
+            <div className="absolute bottom-6 cursor-pointer left-8 md:left-14 lg:left-0 flex gap-10 lg:justify-between w-[100px] md:w-[50px] lg:w-[600px] p-3">
+              {isVideoPlaying ? (
+                <button onClick={handleVideoPress}>
+                  <BsFillPauseFill className="text-black text-2xl lg:text-4xl" />
+                </button>
+              ) : (
+                <button onClick={handleVideoPress}>
+                  <BsFillPlayFill className="text-black text-2xl lg:text-4xl" />
+                </button>
+              )}
+              {isVideoMuted ? (
+                <button onClick={handleVideoSound}>
+                  <HiVolumeOff className="text-black text-2xl lg:text-4xl" />
+                </button>
+              ) : (
+                <button onClick={handleVideoSound}>
+                  <HiVolumeUp className="text-black text-2xl lg:text-4xl" />
+                </button>
+              )}
+            </div>
+          )}
         </div>
       </div>
     </div>
