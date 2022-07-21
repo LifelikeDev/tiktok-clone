6bee70b5abf57fc75ac100933b4e130dd597f869
dev-navian <primeguyy@gmail.com>
2022-07-21 23:59:28 +0000

    upload video display

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -7,15 +7,41 @@ import useAuthStore from "../store/authStore";
 import { client } from "../utils/client";
 
 const Upload = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [videoAsset, setVideoAsset] = useState(null);
+
   return (
     <div className="flex w-full h-full">
       <div className="bg-white rounded-lg">
         <>
           <div>
             <p className="text-2xl font-bold">Upload</p>
-            <p className="text-md text-gray-400 mt-1">Post a video to your account</p>
+            <p className="text-md text-gray-400 mt-1">
+              Post a video to your account
+            </p>
           </div>
-          <div className="border-dashed rounded-xl border-4 border-gray-200 flex flex-col justify-center items-center outline-none mt-10 w-[260px] h-[460px] p-10 cursor-pointer hover:border-red-300 hover:bg-gray-100"></div>
+          <div className="border-dashed rounded-xl border-4 border-gray-200 flex flex-col justify-center items-center outline-none mt-10 w-[260px] h-[460px] p-10 cursor-pointer hover:border-red-300 hover:bg-gray-100">
+            {isLoading ? (
+              <p>Uploading</p>
+            ) : (
+              <div>
+                {videoAsset ? (
+                  <div></div>
+                ) : (
+                  <label className="cursor-pointer">
+                    <div className="flex flex-col items-center justify-center h-full">
+                      <div className="flex flex-col items-center justify-center">
+                        <p className="font-bold text-xl">
+                          <FaCloudUploadAlt className="text-6xl text-gray-300" />
+                        </p>
+                        <p className="text-md font-semibold text-center">Upload Video</p>
+                      </div>
+                    </div>
+                  </label>
+                )}
+              </div>
+            )}
+          </div>
         </>
       </div>
     </div>
