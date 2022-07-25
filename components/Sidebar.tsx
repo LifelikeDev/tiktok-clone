e6dbc9269032cb84367f1b6b8baefae1a1fa7d17
dev-navian <primeguyy@gmail.com>
2022-07-25 23:04:41 +0000

    remove login component from sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -40,30 +40,6 @@ const Sidebar: NextPage = () => {
               </div>
             </Link>
           </div>
-          {!userProfile && (
-            <div className="px-2 py-4 hidden xl:block">
-              <p className="text-gray-400">
-                Login to like and comment on videos
-              </p>
-              <div className="pr-4">
-                <GoogleLogin
-                  clientId=""
-                  render={(renderProps) => (
-                    <button
-                    className="bg-white hover:bg-[#F51997] text-lg text-[#F51997] border-[1px] border-[#F51997] hover:text-white font-semibold px-4 py-2 cursor-pointer rounded-md outline-none w-full mt-3"
-                      onClick={renderProps.onClick}
-                      disabled={renderProps.disabled}
-                    >
-                      Log in
-                    </button>
-                  )}
-                  onSuccess={() => {}}
-                  onFailure={() => {}}
-                  cookiePolicy="single_host_origin"
-                />
-              </div>
-            </div>
-          )}
 
           <Discover />
           <SuggestedAccounts />
