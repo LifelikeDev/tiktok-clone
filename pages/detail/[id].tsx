c8ea6455d1738081985afa654b1e307b5da32da9
dev-navian <primeguyy@gmail.com>
2022-07-29 22:50:43 +0000

    post page structure initialized

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -8,13 +8,29 @@ import { BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
 import { GoVerified } from "react-icons/go";
 import { MdOutlineCancel } from "react-icons/md";
 import { BASE_URL } from "../../utils";
+import { Video } from "../../types.dev";
 
-const Detail = () => {
+
+interface IProps {
+  postDetails: Video
+}
+
+const Detail = ({ postDetails } : IProps) => {
   return <div>Detail</div>;
 };
 
-export const getServerSideProps = async (params: { id: string }) => {
-  const {data} = await axios.get(`${BASE_URL}/api/post/${params.id}`)
-}
+export const getServerSideProps = async ({
+  params: { id },
+}: {
+  params: { id: string };
+}) => {
+  const { data } = await axios.get(`${BASE_URL}/api/post/${id}`);
+
+  return {
+    props: {
+      postDetails: data,
+    },
+  };
+};
 
 export default Detail;
