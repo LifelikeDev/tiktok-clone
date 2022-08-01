96042cceb9cb855b54d4ee9460ae88406cdf2903
dev-navian <primeguyy@gmail.com>
2022-08-01 22:41:36 +0000

    single post video

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -10,13 +10,40 @@ import { MdOutlineCancel } from "react-icons/md";
 import { BASE_URL } from "../../utils";
 import { Video } from "../../types.dev";
 
-
 interface IProps {
-  postDetails: Video
+  postDetails: Video;
 }
 
-const Detail = ({ postDetails } : IProps) => {
-  return <div>Detail</div>;
+const Detail = ({ postDetails }: IProps) => {
+  const [post, setPost] = useState(postDetails);
+  const videoRef = useRef(null);
+
+  const handleVideoClick = () => {};
+
+  if (!post) return null;
+
+  return (
+    <div className="flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap">
+      <div className="relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-[#555]">
+        <div className="opacity-90 absolute top-6 left-2 lg:left-6 flex gap-6 z-50">
+          <p className="cursor-pointer " onClick={() => {}}>
+            <MdOutlineCancel className="text-white text-[35px] hover:opacity-90" />
+          </p>
+        </div>
+        <div className="relative">
+          <div className="lg:h-[100vh] h-[60vh]">
+            <video
+              ref={videoRef}
+              onClick={handleVideoClick}
+              loop
+              src={post?.video?.asset.url}
+              className=" h-full cursor-pointer"
+            ></video>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export const getServerSideProps = async ({
