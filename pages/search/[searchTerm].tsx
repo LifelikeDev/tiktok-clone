86fc629a5a8697d8cb253aa946d40400d77625d7
dev-navian <primeguyy@gmail.com>
2022-08-08 22:10:14 +0000

    search results: accounts and videos tabs

--- pages/search/[searchTerm].tsx
+++ pages/search/[searchTerm].tsx
@@ -12,7 +12,33 @@ import { BASE_URL } from "../../utils";
 import useAuthStore from "../../store/authStore";
 
 const Search = ({ videos }: { videos: Video[] }) => {
-  return <div>Search</div>;
+  const [isAccounts, setIsAccounts] = useState<boolean>(true);
+
+  const accounts = isAccounts
+    ? "border-b-4 border-gray-600"
+    : "text-gray-400";
+  const isVideos = !isAccounts
+    ? "border-b-4 border-gray-600"
+    : "text-gray-400";
+
+  return (
+    <div className="w-full">
+      <div className="flex gap-10 my-10 border-b-2 border-gray-200 bg-white w-full">
+        <p
+          className={`text-xl font-semibold cursor-pointer mt-2 w-20 text-center ${accounts}`}
+          onClick={() => setIsAccounts(true)}
+        >
+          Accounts
+        </p>
+        <p
+          className={`text-xl font-semibold cursor-pointer mt-2 w-20 text-center ${videos}`}
+          onClick={() => setIsAccounts(false)}
+        >
+          Videos
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export const getServerSideProps = async ({
