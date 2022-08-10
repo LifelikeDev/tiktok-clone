088dbe307e1175e53bb96a157c1cbda39fd53140
LifelikeDev <75231564+LifelikeDev@users.noreply.github.com>
2022-08-10 22:46:34 +0000

    search and display videos

--- pages/search/[searchTerm].tsx
+++ pages/search/[searchTerm].tsx
@@ -13,13 +13,11 @@ import useAuthStore from "../../store/authStore";
 
 const Search = ({ videos }: { videos: Video[] }) => {
   const [isAccounts, setIsAccounts] = useState<boolean>(true);
+  const router = useRouter();
+  const { searchTerm } = router.query;
 
-  const accounts = isAccounts
-    ? "border-b-4 border-gray-600"
-    : "text-gray-400";
-  const isVideos = !isAccounts
-    ? "border-b-4 border-gray-600"
-    : "text-gray-400";
+  const accounts = isAccounts ? "border-b-4 border-gray-600" : "text-gray-400";
+  const isVideos = !isAccounts ? "border-b-4 border-gray-600" : "text-gray-400";
 
   return (
     <div className="w-full">
@@ -31,12 +29,24 @@ const Search = ({ videos }: { videos: Video[] }) => {
           Accounts
         </p>
         <p
-          className={`text-xl font-semibold cursor-pointer mt-2 w-20 text-center ${videos}`}
+          className={`text-xl font-semibold cursor-pointer mt-2 w-20 text-center ${isVideos}`}
           onClick={() => setIsAccounts(false)}
         >
           Videos
         </p>
       </div>
+
+      {isAccounts ? (
+        <div>accounts</div>
+      ) : (
+        <div className="md:mt-16 flex flex-wrap gap-6 md:justify-start">
+          {videos.length ? (
+            videos.map((video: Video) => <VideoCard post={video} key={video._id} />)
+          ) : (
+            <NoResults text={`No video results for ${searchTerm}`} />
+          )}
+        </div>
+      )}
     </div>
   );
 };
